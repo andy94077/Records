2d697715d4cadc99de9d72a45345604c281a8c04
andy94077 <andy94077@gmail.com>
2021-12-20 22:13:24 +0800

    Fix wget bug

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mv ~/.vimrc ~/.vimrc_backup
-wget https://raw.githubusercontent.com/andy94077/Records/master/.vimrc_workstation -o ~/.vimrc
+wget https://raw.githubusercontent.com/andy94077/Records/master/.vimrc_workstation -O ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 #cd ~/.vim/bundle/YouCompleteMe
